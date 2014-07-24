06392c05c59e6d8210b864e974fdd014c9e9f4d9
A-Mobile LLC <a-mobilellc@iMac-A-Mobile.local>
2014-07-24 13:30:52 +0600

    init cocoapods

---
+++ Utils/VVServerManager.m
@@ -0,0 +1,17 @@
+//
+//  VVServerManager.m
+//  rentor
+//
+//  Created by A-Mobile LLC on 24.07.14.
+//  Copyright (c) 2014 A-Mobile LLC. All rights reserved.
+//
+
+#import "VVServerManager.h"
+
+@implementation VVServerManager
+
+#pragma mark - GET
+
+#pragma mark - POST
+
+@end
